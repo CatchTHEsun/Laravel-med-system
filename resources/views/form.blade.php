61a8560946ad22dc97aefcb9e4c08c817e7534c7
Andrey <notmymaster@gmail.com>
2021-11-10 22:47:42 +0200

    Validation + (name, email)

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -18,13 +18,19 @@
         <div class="row">
             <div class="col">
                 <input name="name"
-                value="{{ isset($user) ? $user->name : null }}"
+                value="{{ old('name', isset($user) ? $user->name : null) }}"
                 type="text" class="form-control" placeholder="Name" aria-label="Name">
+            @error('name')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             </div>
             <div class="col">
                 <input name="email"
-                value="{{ isset($user) ? $user->email : null }}"
+                value="{{ old('email', isset($user) ? $user->email : null) }}"
                 type="email" class="form-control" placeholder="email" aria-label="email">
+            @error('email')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             </div>
             <div class="col">
                 <button type="submit" class="btn btn-dark">Save</button>
